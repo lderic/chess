ac940d0fe443a6a5cbb47c905e5f3f37d5fb38b6
Eric <lderic0415@gmail.com>
2022-05-17 17:58:07 -0700

    Add dependencies

--- chess-server/build.gradle.kts
+++ chess-server/build.gradle.kts
@@ -10,8 +10,15 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation(kotlin("stdlib"))
+    val ktor_version = "2.0.1"
+    val logback_version = "1.2.11"
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("io.ktor:ktor-server-websockets:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 tasks.getByName<Test>("test") {
