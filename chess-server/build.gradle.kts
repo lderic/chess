88dc961c379ab74122a11bc017d8f27694582daa
Eric <lderic0415@gmail.com>
2022-05-22 21:44:34 -0700

    Update build.gradle

--- chess-server/build.gradle.kts
+++ chess-server/build.gradle.kts
@@ -1,26 +1,33 @@
 plugins {
     id("java")
+    id ("org.jetbrains.kotlin.jvm") version "1.6.20"
 }
 
-group = "com.lderic"
-version = "0.0.0"
+val ktor_version = "2.0.1"
+val logback_version = "1.2.11"
 
-repositories {
-    mavenCentral()
-}
+allprojects {
+    group = "com.lderic"
+    version = "0.0.0"
 
-dependencies {
-    implementation(kotlin("stdlib"))
-    val ktor_version = "2.0.1"
-    val logback_version = "1.2.11"
-    implementation("io.ktor:ktor-server-core:$ktor_version")
-    implementation("io.ktor:ktor-server-netty:$ktor_version")
-    implementation("io.ktor:ktor-server-websockets:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-}
+    apply(plugin = "java")
+    apply(plugin = "org.jetbrains.kotlin.jvm")
 
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        implementation(kotlin("stdlib"))
+        implementation("io.ktor:ktor-server-core:$ktor_version")
+        implementation("io.ktor:ktor-server-netty:$ktor_version")
+        implementation("io.ktor:ktor-server-websockets:$ktor_version")
+        implementation("ch.qos.logback:logback-classic:$logback_version")
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    }
+
+    tasks.getByName<Test>("test") {
+        useJUnitPlatform()
+    }
 }
